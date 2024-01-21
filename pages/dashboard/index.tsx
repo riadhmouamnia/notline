ac292e4b3e14a8ad13690403a6ebb2bb1659adb9
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2024-01-21 07:27:41 +0100

    feat: create dashboard page

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,9 +2,19 @@ import Link from "next/link";
 
 function DashboardPage() {
   return (
-    <div className="flex flex-col min-h-screen justify-between items-center">
-      <Link href="/dashboard/host">Host</Link>
-      <Link href="/dashboard/join">Join</Link>
+    <div className="flex flex-col min-h-screen justify-between items-center md:flex-row">
+      <Link
+        className="flex-1 w-full rounded-sm p-10 text-xl font-bold hover:bg-green-600 hover:text-white flex items-center justify-center"
+        href="/dashboard/join"
+      >
+        Join
+      </Link>
+      <Link
+        className="flex-1 w-full rounded-sm p-10 text-xl font-bold hover:bg-orange-500 hover:text-white flex items-center justify-center"
+        href="/dashboard/host"
+      >
+        Host
+      </Link>
     </div>
   );
 }
