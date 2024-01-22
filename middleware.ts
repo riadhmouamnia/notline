bbb5c3f9fa98cf284fdf78e3eef3fb366b74a9e0
Riadh mouamnia <riad.sodmg@gmail.com>
2024-01-22 21:01:25 +0100

    fix: fix middleware url

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,18 @@
 import { NextApiRequest } from "next";
+import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export default function middleware(req: NextApiRequest | any) {
   let verify = req.cookies.get("loggedin");
-  let url = req.url;
+  const url = req.nextUrl.clone();
 
-  if (!verify && url.includes("/dashboard")) {
-    return NextResponse.redirect("notline.vercel.app/login");
+  if (!verify && url.pathname.includes("/dashboard")) {
+    url.pathname = "/login";
+    return NextResponse.rewrite(url);
   }
 
-  if (verify && url.includes("/login")) {
-    return NextResponse.redirect("notline.vercel.app/");
+  if (verify && url.pathname.includes("/login")) {
+    url.pathname = "/";
+    return NextResponse.rewrite(url);
   }
 }
