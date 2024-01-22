b6cb8be68e6995b817c8568ab25e1bcfb22ce0d9
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2024-01-22 14:23:56 +0100

    fix: redirect URL on deployment

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,15 @@
 import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
-export default function middleware(req: NextApiRequest | any){
-    let verify = req.cookies.get("loggedin");
-    let url = req.url
-    
-    if(!verify && url.includes('/dashboard')){
-        return NextResponse.redirect("http://localhost:3000/login");
-    }
+export default function middleware(req: NextApiRequest | any) {
+  let verify = req.cookies.get("loggedin");
+  let url = req.url;
 
-    if (verify && url.includes('/login')) {
-      return NextResponse.redirect("http://localhost:3000/");
-    }
+  if (!verify && url.includes("/dashboard")) {
+    return NextResponse.redirect("notline.vercel.app/login");
+  }
+
+  if (verify && url.includes("/login")) {
+    return NextResponse.redirect("notline.vercel.app/");
+  }
 }
